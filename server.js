5820047e9c37d724e23e3fd4da4fdfe31ee7b0ed
SteMorel <stefano.morello@live.com>
2022-10-25 10:36:59 +0000

    enabled URL logging console

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ if (dotenv.error) {
 // console.log(`NLU_APIKEY: ${NLU_APIKEY}`);
 // console.log(`NLU_URL: ${NLU_URL}`);
 // console.log(`CLOUDANT_API: ${CLOUDANT_API}`);
-// console.log(`CLOUDANT_URL: ${CLOUDANT_URL}`);
+console.log(`CLOUDANT_URL: ${CLOUDANT_URL}`);
 
 if (CLOUDANT_API && CLOUDANT_URL) {
   var Cloudant = require('@cloudant/cloudant');
